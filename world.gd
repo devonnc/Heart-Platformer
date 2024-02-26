dba11d45dd015a79ef8af301dc7060c5375b6923
devonnc <dechavez@s.sfusd.edu>
2024-02-25 23:29:00 -0800

    HP CP 11 Complete

--- world.gd
+++ world.gd
@@ -4,13 +4,13 @@ extends Node2D
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed():
 	level_completed.show()
 	if not next_level is PackedScene: return
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
